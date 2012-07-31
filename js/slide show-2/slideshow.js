c64647736fbce39c1a37ad522528d78e814846e3
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-08-01 02:43:39 +0430

    create AutoPlay

--- js/slide show-2/slideshow.js
+++ js/slide show-2/slideshow.js
@@ -3,6 +3,7 @@ window.onload=function(){
 	var train=slideshow.getElementsByClassName('train').item(0);
 	var list=slideshow.getElementsByClassName('btn').item(0).getElementsByTagName('li');
 	
+
 	for (var i=0;i<list.length;i++){
 		(function(j){
 			list.item(j).onclick=function(){
@@ -11,8 +12,20 @@ window.onload=function(){
 		})(i)
 	}
 	
+	var currentSlide=0;
 	function goToSlide(n){
+		if (n > list.length-1) n=0;
 		train.style.left=(-570*n)+'px';
+		list.item(currentSlide).className='';
+		list.item(n).className='active';
+		currentSlide=n;
 	}
 
+	var nextSlide=function(){
+		goToSlide(currentSlide+1);
+	}
+
+	goToSlide(0);
+
+	setInterval(nextSlide,5000);
 }
