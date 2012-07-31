373559a3ebe3780bfc0b45dab0c84f25da3080da
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-08-01 02:29:09 +0430

    add js list

--- js/slide show-2/slideshow.js
+++ js/slide show-2/slideshow.js
@@ -1,3 +1,18 @@
 window.onload=function(){
+	var slideshow=document.getElementsByClassName('slideshow').item(0);
+	var train=slideshow.getElementsByClassName('train').item(0);
+	var list=slideshow.getElementsByClassName('btn').item(0).getElementsByTagName('li');
 	
+	for (var i=0;i<list.length;i++){
+		(function(j){
+			list.item(j).onclick=function(){
+				goToSlide(j);
+			}
+		})(i)
+	}
+	
+	function goToSlide(n){
+		train.style.left=(-570*n)+'px';
+	}
+
 }
