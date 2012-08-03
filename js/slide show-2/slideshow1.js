6569e2ceac101a15b08fb51d70481555776e35f8
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-08-03 14:35:56 +0430

    add autoplay with stop & start

--- js/slide show-2/slideshow1.js
+++ js/slide show-2/slideshow1.js
@@ -2,12 +2,37 @@ window.onload=function(){
 	var slideshow=document.getElementsByClassName('slideshow').item(0);
 	var train=slideshow.getElementsByClassName('train').item(0);
 	var list=slideshow.getElementsByClassName('btn').item(0).getElementsByTagName('li');
+	var currentSlide=0;
+	var play=false;
+
+	function gotoslide(n){
+		if (n > list.length-1) n=0;
+		train.style.left=(-570*n)+'px';
+		list.item(currentSlide).className='';
+		list.item(n).className='active';
+		currentSlide=n;
+	}
+
+	function nextslide(){
+		gotoslide(currentSlide+1);
+	}
+
+	autoPlay= slideshow.onmouseout=function(){
+		play= setInterval(nextslide,2000);
+	}
+
+	autoStop= slideshow.onmouseover=function(){
+		clearInterval(play);
+	}
 
 	for (var i=0;i<list.length;i++){
 		(function(j){
 			list.item(j).onclick=function(){
-				alert(j);
+				gotoslide(j);
 			}
 		})(i)
 	}
+
+	gotoslide(0);
+	autoPlay();
 }
