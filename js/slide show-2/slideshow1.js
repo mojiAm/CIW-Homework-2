f5adf555df8675d588d8b38779a38ab183cc7150
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-08-03 13:49:43 +0430

    review cods

---
+++ js/slide show-2/slideshow1.js
@@ -0,0 +1,13 @@
+window.onload=function(){
+	var slideshow=document.getElementsByClassName('slideshow').item(0);
+	var train=slideshow.getElementsByClassName('train').item(0);
+	var list=slideshow.getElementsByClassName('btn').item(0).getElementsByTagName('li');
+
+	for (var i=0;i<list.length;i++){
+		(function(j){
+			list.item(j).onclick=function(){
+				alert(j);
+			}
+		})(i)
+	}
+}
