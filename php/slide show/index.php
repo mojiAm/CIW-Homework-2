cd7e64e690c9e137424a62781811baef0e9f80b7
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-08-03 22:28:22 +0430

    slide show with php

---
+++ php/slide show/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="reset.css" />
+	<link rel="stylesheet" type="text/css" href="slideshow.css" />
+	<script type="text/javascript" src="slideshow1.js"></script>
+	<title></title>
+</head>
+<body>
+	<div class="container">
+		<div class="frame">
+			<div class="prev"></div>
+			<div class="next"></div>
+			<div class="new"></div>
+			<div class="slideshow">
+				<div class="train">
+					<?php
+					$files= scandir('./images/');
+					$counter=0;
+					foreach ($files as $file) {
+						$exe= end(explode('.',$file));
+						if ($exe == 'jpg'){
+							echo '<div style="background-image:url(./images/$file);"></div>';
+							$counter++;
+						}
+					}
+					
+					?>
+				</div>
+				<ul class="btn">
+					<?php
+						for ($i=0; $i < $counter; $i++) { 
+							echo "<li></li>";
+						}
+					?>					
+				</ul>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
