e32133b65e7822c9d420d0fc5b83e422fae95cd5
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-08-09 04:06:41 +0430

    fix some problem

--- js/slide show-2/slideshow1.js
+++ js/slide show-2/slideshow1.js
@@ -1,11 +1,12 @@
 window.onload=function(){
-	var frame=document.getElementsByClassName('frame').item(0);
-	var next=frame.getElementsByClassName('next').item(0);
-	var prev=frame.getElementsByClassName('prev').item(0);
-	var slideshow=document.getElementsByClassName('slideshow').item(0);
-	var train=slideshow.getElementsByClassName('train').item(0);
-	var list=slideshow.getElementsByClassName('btn').item(0).getElementsByTagName('li');
-	var currentSlide=0;
+	var frame= document.getElementsByClassName('frame').item(0);
+	var next= frame.getElementsByClassName('next').item(0);
+	var prev= frame.getElementsByClassName('prev').item(0);
+	var slideshow= document.getElementsByClassName('slideshow').item(0);
+	var train= slideshow.getElementsByClassName('train').item(0);
+	var list= slideshow.getElementsByClassName('btn').item(0).getElementsByTagName('li');
+	var play= false;
+	var currentSlide= 0;
 
 	function gotoslide(n){
 		if (n > list.length-1) n=0;
@@ -13,7 +14,7 @@ window.onload=function(){
 		train.style.left=(-570*n)+'px';
 		list.item(currentSlide).className='';
 		list.item(n).className='active';
-		currentSlide=n;
+		currentSlide= n;
 	}
 
 	function nextslide(){
@@ -26,15 +27,16 @@ window.onload=function(){
 
 	prevslide= prev.onclick=function(){
 		gotoslide(currentSlide-1);
-		
 	}
 
 	autoPlay= frame.onmouseout=function(){
+		if (play) return;
 		play= setInterval(nextslide,5000);
 	}
 
 	autoStop= frame.onmouseover=function(){
 		clearInterval(play);
+		play= false;
 	}
 
 	for (var i=0;i<list.length;i++){
