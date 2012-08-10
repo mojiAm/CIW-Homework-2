572db3d5efb5e789f976af607274bfa57092f41c
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-08-10 15:29:24 +0430

    add events

--- php/Acc Slide/Accslide.js
+++ php/Acc Slide/Accslide.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
 	slides= $('div.slideshow .train > div');
-	var width= Math.ceil(960/5);
+	var widths= Math.ceil(1160/5);
 
+	slides.css({width:widths+'px'});
 	
+
+	slides.mouseover(function(){
+		slides.css({width:'50px'});
+		$(this).css({width:'960px'});
+	})
+	slides.mouseout(function(){
+		slides.css({width:widths+'px'});
+	})
 })
