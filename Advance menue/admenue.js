f06819b16899b9e363c526e30320f55ed4abb577
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-08-24 19:57:40 +0430

    add zepto code

--- Advance menue/admenue.js
+++ Advance menue/admenue.js
@@ -1,3 +1,30 @@
 $(function(){
+	var lis= $('nav.menu li a');
 	
+	var step=500;
+	lis.each(function(){
+		$(this).animate({
+			top:-79
+		},step,'ease-in-out');
+		step+=300;
+	});
+
+	lis.each(function(){
+		var that=$(this);
+		$(this).mouseover(function(){
+			that.animate({
+				top:0
+			},500,'ease-in')
+		});
+	});
+
+	lis.each(function(){
+	var that=$(this);
+	$(this).mouseout(function(){
+			that.animate({
+				top:-79
+			},500,'ease-out')
+		});
+	});
+
 });
