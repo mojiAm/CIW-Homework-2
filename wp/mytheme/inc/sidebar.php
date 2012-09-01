ab63c6e381fa49c9964cdead58a01c83288a138e
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-09-02 01:33:51 +0430

    create theme in Wordpress

---
+++ wp/mytheme/inc/sidebar.php
@@ -0,0 +1,14 @@
+<div class="sidebar1">
+  <ul class="nav">
+    <li><a href="./">Home</a></li>
+    <li><a href="product.php">Product</a></li>
+    <li><a href="services.php">Services</a></li>
+    <li><a href="about.php">About Us</a></li>
+  </ul>
+  <p>
+    The above links demonstrate a basic navigational structure using an unordered list styled with CSS. Use this as a starting point and modify the properties to produce your own unique look. If you require flyout menus, create your own using a Spry menu, a menu widget from Adobe's Exchange or a variety of other javascript or CSS solutions.
+  </p>
+  <p>
+    If you would like the navigation along the top, simply move the ul.nav to the top of the page and recreate the styling.
+  </p>
+</div>
---
+++ wp/mytheme/index.php
@@ -0,0 +1,15 @@
+<?php
+  get_template_part('inc/header')
+?>
+
+<?php
+  get_template_part('inc/sidebar')
+?>
+
+<?php
+  get_template_part('inc/content')
+?>
+
+<?php
+    get_template_part('inc/footer')
+?>
