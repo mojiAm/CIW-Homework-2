af59b05812367bb2d4d5466023d5793f977a77d3
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-09-13 02:03:59 +0430

    create my theme

---
+++ wp/mytheme/404.php
@@ -0,0 +1,16 @@
+<?php
+    get_template_part('inc/header');
+
+    get_template_part('inc/sidebar','left');
+?>
+
+  	<article>
+		<h1> 404 </h1>
+		<p> The page not found!!! </p>   	
+	</article> 
+
+<?php
+    get_template_part('inc/sidebar','right');
+
+    get_template_part('inc/footer');
+?>
---
+++ wp/mytheme/index.php
@@ -0,0 +1,11 @@
+<?php
+  get_template_part('inc/header');
+
+  get_template_part('inc/sidebar','left');
+
+  get_template_part('inc/content');
+
+  get_template_part('inc/sidebar','right');
+
+  get_template_part('inc/footer');
+?>
