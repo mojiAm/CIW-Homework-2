e30a347f877a30231328be84cabb331367f934c8
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-09-14 18:08:36 +0430

    add dynamic field

--- wp/mytheme/inc/content.php
+++ wp/mytheme/inc/content.php
@@ -1,10 +1,17 @@
 	<div class="content">
 		<?php if (have_posts()) {
 					the_post();
+					$meta= get_post_custom();
 		?>
 					<article>
 						<h1><?php the_title(); ?></h1>
 						<p><?php the_content(); ?></p>   	
+						<?php
+							if (count($meta['price'])>0){
+								$price= $meta['price'][0];
+								echo "<p>Price: $price $</p>";
+							}
+						?>
 					</article>
 		<?php } ?>
 
